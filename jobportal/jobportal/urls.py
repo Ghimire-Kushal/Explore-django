9bf4021512004dfd708e34abee2265d3c421287a
Kushal Ghimire <kushal.upr@gmail.com>
2026-09-13 18:00:00 +0545

    Update project

--- jobportal/jobportal/urls.py
+++ jobportal/jobportal/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import include, path
 from .import views
 
-# urlpatterns created to map the URL patterns to the corresponding views in the jobportal application. Each path function defines a route and associates it with a view function that will handle the request for that route.
+# urlpatterns created to map the URL patterns to the corresponding views inyhr  jobportal application. Each path function defines a route and associates it with a view function that will handle the request for that route.
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
